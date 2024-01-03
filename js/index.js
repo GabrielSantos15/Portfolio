b97c2e59fb7c425647e8b3dcf3e91e37f2b15996
Gabriel dos Santos <gs303291@gmail.com>
2024-01-03 19:54:57 -0300

    criando o portfolio

---
+++ js/index.js
@@ -0,0 +1,36 @@
+function recuperarDarkMode() {
+  const storageDark = localStorage.getItem("dark");
+  if (storageDark) darkMode();
+}
+
+recuperarDarkMode();
+
+function darkMode() {
+  const body = document.querySelector("body");
+  const buttonDarkMode = document.querySelector("#buttonDarkMode");
+
+  localStorage.clear();
+
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+
+    buttonDarkMode.classList.remove('fa-moon')
+    buttonDarkMode.classList.add('fa-sun')
+
+  } else {
+    body.classList.add("dark");
+    localStorage.setItem("dark", 1);
+
+    buttonDarkMode.classList.remove('fa-sun')
+    buttonDarkMode.classList.add('fa-moon')
+   
+  }
+}
+
+// menu mobile
+
+function menu(){
+    const menuMobile = document.querySelector('#menuMobile')
+
+    menuMobile.classList.contains('hide') ? menuMobile.classList.remove('hide') :  menuMobile.classList.add('hide')
+}
