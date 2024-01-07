c6f141aa57552936dc2019d0f77b41289742149f
Gabriel dos Santos <gs303291@gmail.com>
2024-01-07 20:25:57 -0300

    adicionando meta descrição

--- js/index.js
+++ js/index.js
@@ -14,23 +14,23 @@ function darkMode() {
   if (body.classList.contains("dark")) {
     body.classList.remove("dark");
 
-    buttonDarkMode.classList.remove('fa-moon')
-    buttonDarkMode.classList.add('fa-sun')
-
+    buttonDarkMode.classList.remove("fa-moon");
+    buttonDarkMode.classList.add("fa-sun");
   } else {
     body.classList.add("dark");
     localStorage.setItem("dark", 1);
 
-    buttonDarkMode.classList.remove('fa-sun')
-    buttonDarkMode.classList.add('fa-moon')
-   
+    buttonDarkMode.classList.remove("fa-sun");
+    buttonDarkMode.classList.add("fa-moon");
   }
 }
 
 // menu mobile
 
-function menu(){
-    const menuMobile = document.querySelector('#menuMobile')
+function menu() {
+  const menuMobile = document.querySelector("#menuMobile");
 
-    menuMobile.classList.contains('hide') ? menuMobile.classList.remove('hide') :  menuMobile.classList.add('hide')
+  menuMobile.classList.contains("hide")
+    ? menuMobile.classList.remove("hide")
+    : menuMobile.classList.add("hide");
 }
