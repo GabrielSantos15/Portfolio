a1830f069e87333e71aa25de4ed7e2eb67177b13
Gabriel dos Santos <gs303291@gmail.com>
2025-12-01 23:14:59 -0300

    feat: criação da nova home e sessão sobre

---
+++ src/components/About/index.jsx
@@ -0,0 +1,27 @@
+import Foto from "../../assets/Gabriel_foto.png"
+import './About.estilos.css'
+import Curriculo from "../../assets/Currículo_Gabriel.pdf"
+
+export default function About() {
+  return (
+    <section id="AboutSection">
+      <figure>
+        <img src={Foto} width={700} alt="" />
+      </figure>
+      <article>
+        <h1>Gabriel dos Santos</h1>
+        <p>
+          Minha atuação vai além da estética: foco na engenharia por trás da
+          interface, priorizando <strong> acessibilidade, SEO e performance</strong> para garantir
+          que o produto não seja apenas bonito, mas funcional e escalável. Busco
+          oportunidades onde possa aplicar conceitos de Clean Code e tecnologias
+          modernas como React para resolver problemas reais de usuários e
+          negócios
+        </p>
+        <a href={Curriculo} download>
+          <button id="downloadCv">Download CV</button>
+          </a>
+      </article>
+    </section>
+  );
+}
