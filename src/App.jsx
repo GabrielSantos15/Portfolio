672969ec605975be3ba11eb55cb17f15637fcfe0
Gabriel dos Santos <gs303291@gmail.com>
2025-12-23 11:23:20 -0300

    fix: corrigindo rota do Skills

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Timeline from "./components/Timeline/INDEX.JSX";
+import Timeline from "./components/Timeline";
 
 import { formacao } from "./data/formacao";
 import { experiencia } from "./data/experiencia";
